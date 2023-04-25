a1b4cc323571ec3f193bb7995691ba67146f2cca
Abdelaziz Khedr <Azkh@gmail.com>
2023-04-25 20:26:13 +0200

    Created Strings2 file

---
+++ Strings2.c
@@ -0,0 +1,128 @@
+#include "main.h"
+
+/**
+ * _strdup - duplicates str in the heap.
+ * @str: Type char pointer str
+ * Return: duplicated str
+ */
+char *_strdup(const char *str)
+{
+	char *new;
+	size_t length;
+
+	length = _strlen(str);
+	new = malloc(sizeof(char) * (length + 1));
+	if (new == NULL)
+		return (NULL);
+	_memcpy(new, str, length + 1);
+	return (new);
+}
+
+/**
+ * _strlen - Returns the lenght of a string.
+ * @str: Type char pointer
+ * Return: Always 0.
+ */
+int _strlen(const char *str)
+{
+	int length;
+
+	for (length = 0; str[length] != 0; length++)
+	{
+	}
+	return (length);
+}
+
+/**
+ * cmp_chars - compare characters of strings
+ * @str: input string.
+ * @delimit: delimiter.
+ *
+ * Return: 1 if are equals, 0 if not.
+ */
+int cmp_chars(char str[], const char *delimit)
+{
+	unsigned int i, j, k;
+
+	for (i = 0, k = 0; str[i]; i++)
+	{
+		for (j = 0; delimit[j]; j++)
+		{
+			if (str[i] == delimit[j])
+			{
+				k++;
+				break;
+			}
+		}
+	}
+	if (i == k)
+		return (1);
+	return (0);
+}
+
+/**
+ * _strtok - splits a string by some delimiter.
+ * @str: input string.
+ * @delimit: delimiter.
+ *
+ * Return: string splited.
+ */
+char *_strtok(char str[], const char *delimit)
+{
+	static char *splitted, *str_end;
+	char *str_start;
+	unsigned int i, bool;
+
+	if (str != NULL)
+	{
+		if (cmp_chars(str, delimit))
+			return (NULL);
+		splitted = str;
+		i = _strlen(str);
+		str_end = &str[i];
+	}
+	str_start = splitted;
+	if (str_start == str_end)
+		return (NULL);
+
+	for (bool = 0; *splitted; splitted++)
+	{
+		if (splitted != str_start)
+			if (*splitted && *(splitted - 1) == '\0')
+				break;
+		for (i = 0; delimit[i]; i++)
+		{
+			if (*splitted == delimit[i])
+			{
+				*splitted = '\0';
+				if (splitted == str_start)
+					str_start++;
+				break;
+			}
+		}
+		if (bool == 0 && *splitted)
+			bool = 1;
+	}
+	if (bool == 0)
+		return (NULL);
+	return (str_start);
+}
+
+/**
+ * _isdigit - defines if string passed is a number
+ *
+ * @str: input string
+ * Return: 1 if string is a number. 0 in other case.
+ */
+int _isdigit(const char *str)
+{
+	unsigned int i;
+
+	for (i = 0; str[i]; i++)
+	{
+		if (str[i] < 48 || str[i] > 57)
+			return (0);
+	}
+	return (1);
+}
+
