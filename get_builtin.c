d37dbf919e8a04a0aab7dafbbec36d380c890cf9
Karim Abuzaid <eng.karimedhat@gmail.com>
2023-04-25 18:49:17 -0700

    adding get_builtin file

---
+++ get_builtin.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * Get_built_in - built in that pais in the command arguments
+ * @cmd: command
+ * Return: function pointer of the built in command
+ */
+int (*Get_built_in(char *cmd))(Data_shell *)
+{
+	builtin_t builtin[] = {
+		{ "env", _env },
+		{ "exit", exit_shell },
+		{ "setenv", _setenv },
+		{ "unsetenv", _unsetenv },
+		{ "cd", cd_shell },
+		{ "help", get_help },
+		{ NULL, NULL }
+	};
+	int i;
+
+	for (i = 0; builtin[i].name; i++)
+	{
+		if (_strcmp(builtin[i].name, cmd) == 0)
+			break;
+	}
+
+	return (builtin[i].f);
+}
