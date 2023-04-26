7678508347bda2b5b0459ecb2af07b88e750a943
Karim Abuzaid <eng.karimedhat@gmail.com>
2023-04-25 18:55:25 -0700

    adding ShellExit file

---
+++ ShellExit.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * ShellExit - exits the shell
+ *
+ * @Datashell: data relevant (stat and argss)
+ * Return: 0 on success.
+ */
+int ShellExit(data_shell *Datashell)
+{
+	unsigned int ustatus;
+	int is_digit;
+	int str_len;
+	int big_number;
+
+	if (Datashell->argss[1] != NULL)
+	{
+		ustatus = _atoi(Datashell->argss[1]);
+		is_digit = _isdigit(Datashell->argss[1]);
+		str_len = _strlen(Datashell->argss[1]);
+		big_number = ustatus > (unsigned int)INT_MAX;
+		if (!is_digit || str_len > 10 || big_number)
+		{
+			get_error(Datashell, 2);
+			Datashell->stat = 2;
+			return (1);
+		}
+		Datashell->stat = (ustatus % 256);
+	}
+	return (0);
+}
